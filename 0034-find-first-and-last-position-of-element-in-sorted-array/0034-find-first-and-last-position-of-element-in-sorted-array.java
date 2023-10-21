54cfec33c64b4f25b4a7c837beb44116419cf441
Chetan-Singhai <119690651+Chetan-Singhai@users.noreply.github.com>
2023-10-21 07:46:33 +0530

    Time: 0 ms (100%), Space: 44.4 MB (34.78%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,14 +1,53 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-         int first = -1, last = -1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                if (first == -1) {
-                    first = i;
-                }
-                last = i;
+        //  int first = -1, last = -1;
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] == target) {
+        //         if (first == -1) {
+        //             first = i;
+        //         }
+        //         last = i;
+        //     }
+        // }
+        // return new int[]{first, last};
+        int arr[] =new int[]{-1,-1};
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while (start <= end) { // Use <= to handle the case when start and end meet
+            int mid = start + (end - start) / 2; // Calculate the mid index correctly
+
+            if (nums[mid] == target) {
+                arr[0] = mid;
+                end = mid - 1;
+            } 
+            else if (nums[mid] > target) {
+                end = mid - 1;
+
+              }
+              else {
+                start = mid + 1;
             }
         }
-        return new int[]{first, last};
+
+        start = 0;
+         end = nums.length - 1;
+        
+        while (start <= end) { // Use <= to handle the case when start and end meet
+            int mid = start + (end - start) / 2; // Calculate the mid index correctly
+
+            if (nums[mid] == target) {
+                arr[1] = mid;
+                start = mid + 1;
+            } 
+            else if (nums[mid] > target) {
+                end = mid - 1;
+
+              }
+              else {
+                start = mid + 1;
+            }
+        }
+        return arr ;
     }
 }
